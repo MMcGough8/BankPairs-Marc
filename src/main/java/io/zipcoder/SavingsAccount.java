351ad2309ae9c6b8d87621a45972553849dac22d
FrankMontgomery063 <montgomeryfrank.fm@gmail.com>
2025-11-19 12:01:36 -0500

    "Completed BankPairs"

--- src/main/java/io/zipcoder/SavingsAccount.java
+++ src/main/java/io/zipcoder/SavingsAccount.java
@@ -5,26 +5,27 @@ public class SavingsAccount extends Account {
 
     public SavingsAccount(Object accountHolder, Double balance, String accountNumber, Double interestRate) {
         super(accountHolder, balance, accountNumber);
-        // TODO: Implement constructor
+        this.interestRate = interestRate;
     }
 
     public Double getInterestRate() {
-        // TODO: Implement getter
-        return null;
+        
+        return interestRate;
     }
 
     public void setInterestRate(Double interestRate) {
-        // TODO: Implement setter
+        this.interestRate = interestRate;
     }
 
     public void applyInterest() {
-        // TODO: Implement method to apply interest to the balance
-        // New balance = current balance + (current balance * interest rate)
+        this.balance = this.balance + (this.balance * interestRate);
     }
 
     @Override
     public void debit(Double amount) {
-        // TODO: Implement debit method
-        // Savings accounts have overdraft protection - don't allow balance to go negative
+       if (amount > getBalance()) {
+            return;
+    }
+        super.debit(amount);
     }
 }
